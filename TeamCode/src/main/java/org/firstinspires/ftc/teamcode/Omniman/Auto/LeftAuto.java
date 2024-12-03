af20c7685478b3971d8844c41696559898d31c1b
WeberSchool1 <rambots@weberschool.org>
2024-12-03 08:32:05 -0500

    Auto Arms move and first bucket position works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omniman/Auto/LeftAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omniman/Auto/LeftAuto.java
@@ -46,50 +46,6 @@ public class LeftAuto extends LinearOpMode {
                 man.LinearTargetPos(0);
                 man.delay(2);
                 man.intakePower(-1);
-              /*  man.delay(3);
-                //Grab Second Sample
-                Action Move1stSample=drive.actionBuilder(new Pose2d(-54,-54,Math.toRadians(135)))
-                        .splineToConstantHeading(new Vector2d(-34,-34),Math.toRadians(135))
-                        .splineToConstantHeading(new Vector2d(-40,-20),Math.toRadians(135))
-                        .build();
-                Actions.runBlocking(Move1stSample);
-                man.delay(2);
-                //Score Second Sample
-                Action MoveHB2ND = drive.actionBuilder(new Pose2d(-40, -20,Math.toRadians(135)))
-                        .splineToConstantHeading(new Vector2d(-54, -54), Math.toRadians(135))
-                        .build();
-                Actions.runBlocking(MoveHB2ND);
-                man.delay(3);
-                //Grab 3rd Sample
-                Action Move2NDSample=drive.actionBuilder(new Pose2d(-54, -54, Math.toRadians(135)))
-                        .splineToConstantHeading(new Vector2d(-54, -20), Math.toRadians(135))
-                        .build();
-                Actions.runBlocking(Move2NDSample);
-                man.delay(2);
-                //Score 3rd Sample
-                Action MoveHB3RD=drive.actionBuilder(new Pose2d(-54, -20, Math.toRadians(135)))
-                        .splineToConstantHeading(new Vector2d(-54, -54), Math.toRadians(135))
-                        .build();
-                Actions.runBlocking(MoveHB3RD);
-                man.delay(3);
-                //Grab 4th Sample
-                Action Move4thSample=drive.actionBuilder(new Pose2d(-54,-54,Math.toRadians(135)))
-                        .splineToConstantHeading(new Vector2d(-65, -20),Math.toRadians(135))
-                        .build();
-                Actions.runBlocking(Move4thSample);
-                man.delay(2);
-                //Score 4th Sample
-                Action MoveHB4th=drive.actionBuilder(new Pose2d(-65, -20, Math.toRadians(135)))
-                        .splineToConstantHeading(new Vector2d(-54, -54), Math.toRadians(135))
-                        .build();
-                Actions.runBlocking(MoveHB4th);
-                man.delay(3);
-                //Park
-                Action Park=drive.actionBuilder(new Pose2d(-54, -54, Math.toRadians(135)))
-                        .strafeToLinearHeading(new Vector2d(-25, -11), Math.toRadians(360))
-                        .build();
-                Actions.runBlocking(Park);
-                //End Movement code*/
                 //Arm Code Start
                 xPos=p.position.x;
                 yPos=p.position.y;
